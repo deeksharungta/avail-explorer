4c41f67c5f2ae5ec31a65ae2a6d733fff94e3bff
Deeksha Rungta <deeksharu1902@gmail.com>
2025-03-15 15:28:23 +0400

    style: add active link styling to header navigation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,12 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import WalletConnect from '../wallet/WalletConnect';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <nav className='bg-background text-white py-4'>
       <div className='container mx-auto px-4 flex justify-between items-center'>
@@ -11,8 +14,24 @@ export default function Header() {
           <Link href='/'>
             <Image src='/logo.svg' alt='Logo' width={100} height={100} />
           </Link>
-          <Link href='/'>Actions</Link>
-          <Link href='/explorer'>Explorer</Link>
+          <Link
+            href='/'
+            className={`transition-opacity duration-200 ${
+              pathname === '/' ? 'opacity-100' : 'opacity-60 hover:opacity-100'
+            }`}
+          >
+            Actions
+          </Link>
+          <Link
+            href='/explorer'
+            className={`transition-opacity duration-200 ${
+              pathname === '/explorer'
+                ? 'opacity-100'
+                : 'opacity-60 hover:opacity-100'
+            }`}
+          >
+            Explorer
+          </Link>
         </div>
         <WalletConnect />
       </div>
